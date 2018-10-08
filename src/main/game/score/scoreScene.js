1a6dfcaff9bbb0d362c4f770c1b304418c9ff6a1
Scott Weidenkopf <sweidenkopf@gmail.com>
2018-10-08 19:03:18 -0400

    fixed score text size

--- src/main/game/score/scoreScene.js
+++ src/main/game/score/scoreScene.js
@@ -1,15 +1,12 @@
 import Phaser from 'phaser';
 import RegistryFields from '../registryFields';
 import TextConstants from '../textConstants';
+import {getTextOptions} from '../phaserUtils';
 
 const scoreX = 60;
 const scoreY = 60;
 
-const scoreStyle = {
-    fontSize: TextConstants.BODY_TEXT_SIZE,
-    color: TextConstants.PRIMARY_COLOR,
-    fontFamily: TextConstants.FONT_FAMILY
-};
+const scoreStyle = getTextOptions(TextConstants.SCORE_TEXT_SIZE);
 
 const updateScore = function(score) {
     this.scoreTextField.setText('SCORE: ' + score.toString().padStart(6, '0'));
--- src/main/game/textConstants.js
+++ src/main/game/textConstants.js
@@ -3,6 +3,7 @@ export default Object.freeze({
     TITLE_SIZE: '72px',
     MENU_OPTION_SIZE: '48px',
     BODY_TEXT_SIZE: '24px',
+    SCORE_TEXT_SIZE:'36px',
     PRIMARY_COLOR: '#FFFFFF',
     OUTLINE_COLOR: '#000000',
     MARGIN_X: 30,
